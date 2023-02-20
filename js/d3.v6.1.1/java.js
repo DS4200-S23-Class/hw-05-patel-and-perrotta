a82e9072a6c286e7acd0cf6576c0f90c0a945300
Mitra Patel <mitrapatel@Mitras-MacBook-Pro.local>
2023-02-20 16:22:32 -0500

    graphs working locally

--- js/d3.v6.1.1/java.js
+++ js/d3.v6.1.1/java.js
@@ -1,154 +1,214 @@
-// Frame dimensions set up as variables 
-const FRAME_HEIGHT = 400;
-const FRAME_WIDTH = 400; 
-const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
+// Setting up constants 
+const FRAME_HEIGHT = 550;
+const FRAME_WIDTH = 600;
+const MARGINS = {left: 50, right: 50,
+				top: 50, bottom: 50};
 
+// Constants for scaling purposes
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
-const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.bottom;
+const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
 
- // Mouseover function
- function handleMouseOver(event, d) {
-	d3.select(this).style("fill", "#00c");
-}
+// create a frame for first vis in column 1
+const FRAME1 = d3.select("#flex_left")
+					.append("svg")
+						.attr("height", FRAME_HEIGHT)
+						.attr("width", FRAME_WIDTH)
+						.attr("class", "frame");
 
-// MouseLeave function
-function handleMouseLeave(event, d) {
-	d3.select(this).style("fill", "#CC0000");
-}
+// read data from first file
+d3.csv("data/scatter-data.csv").then((data) => {
 
-// Add border to points if it is not there and remove border if it is there and show coordinate of point
-function onClick(event, d) {
-	// If there is border, remove border and show coordinates
-	if (d3.select(this).attr("stroke") !== "none") {
-        d3.select(this).attr("stroke", "none")
-        d3.select(this).attr("stroke-width", "5")
+	console.log(data);
 
-        // console.log(typeof d.x)
+	// get maximum x and y coordinate values 
+	const X_MAX = d3.max(data, d => {return parseInt(d.x)});
+	const Y_MAX = d3.max(data, d => {return parseInt(d.y)});
 
-        if (typeof d.x !== 'string') {
-        	d3.select("#showPoint").text("Last Point Clicked: (" + x_coord + "," + y_coord + ")")
-        } else {
-			d3.select("#showPoint").text("Last Point Clicked: (" + d.x + "," + d.y + ")")
+	// Scaling x coordinates 
+	const X_SCALE = d3.scaleLinear()
+						.domain([0, (X_MAX)])
+	    				.range([0, (Y_MAX * 50)]);
 
-        }
-	    // Otherwise, show border and coordinates
-	    } else {
-	        d3.select(this).attr("stroke", "black")
-	       	d3.select(this).attr("stroke-width", "5")
+	// Scaling y coordinates
+	const Y_SCALE = d3.scaleLinear()
+						.domain([0, (Y_MAX)])
+	    				.range([(Y_MAX * 50), 0]);
 
-			if (typeof d.x !== 'string') {
-        		d3.select("#showPoint").text("Last Point Clicked: (" + x_coord + "," + y_coord + ")")
-			} else {
-				d3.select("#showPoint").text("Last Point Clicked: (" + d.x + "," + d.y + ")")
+	// plot points from csv file 
+	FRAME1.selectAll(".point")
+					.data(data)
+	    			.enter().append("circle")
+	    						.attr("class", "point")
+	    						.attr("cx", d => {
+	    								return X_SCALE(parseInt(d.x)) + MARGINS.left
+	    							})
+	    						.attr("cy", d => {
+	    								return Y_SCALE(parseInt(d.y)) + MARGINS.top
+	    						})
+	    						.attr("r", 10);
 
-			        }
-		    };
-		};
+	// x-axis creation
+	FRAME1.append("g")
+      		.attr("transform", "translate(" + 
+      			MARGINS.left+ "," + (MARGINS.top + VIS_HEIGHT) + ")")
+      			.call(d3.axisBottom(X_SCALE).ticks(10));
 
+	// y-axis creation
+	FRAME1.append("g")
+      		.attr("transform", "translate(" + 
+      			MARGINS.left + "," + (MARGINS.top) + ")")
+      		.call(d3.axisLeft(Y_SCALE).ticks(10));
 
-function pointClicked(point){
-	// variable to store the point
-	let clickedPt = document.getElementById(point);
+	// handles functions for when a circle is clicked 
+	function pointClicked() {
+		
+		// event listener 
+		this.classList.toggle("addBorder");
+		this.classList.toggle("point");
 
-	// variable to store text that tells user the last point clicked
-	let newText = "Last Point Clicked: " + point;
+		// obtain x and y coordinates
+		let x_var = (this.getAttribute("cx") / 50) - 1;
+		let y_var = (500 - this.getAttribute("cy")) / 50;
 
-	// assign the variable to the id 
-	document.getElementById("showPoint").innerHTML = newText;
-}
+		// stores text of last clicked circle / point
+		let text1 = "Last point clicked: "
+		let text2 = "(" + x_var +"," + y_var + ")"
+		
+		// updates the text for the last clicked circle 
+		document.getElementById("point1").innerHTML = text1;
+		document.getElementById("point2").innerHTML = text2;
+	}
 
-const FRAME1 = d3.select("#vis1") 
-                  .append("svg") 
-                    .attr("height", FRAME_HEIGHT)   
-                    .attr("width", FRAME_WIDTH)
-                    .attr("class", "frame"); 
+	// Create list of points
+	let circles = document.getElementsByTagName("circle");
 
-const xScale = d3.scaleLinear()
-.range([MARGINS.left, FRAME_WIDTH - MARGINS.right]);
+	// loop through all points
+	for (let i = 0; i < circles.length; i++) {
+		
+	    // check each point for clicks
+	    let circle = circles[i];
+	    circle.addEventListener("click", pointClicked);
+	}
 
-const yScale = d3.scaleLinear()
-.range([FRAME_HEIGHT - MARGINS.bottom, MARGINS.top]);
+	function addPoint() {
+	// get the user input selections
+	let xInput = document.getElementById("x_coordinate");
+	let yInput = document.getElementById("y_coordinate");
 
-function scatterPlot() {
-	// reading data from scatter plot csv 
-	d3.csv("data/scatter-data.csv").then((data) => {
+	// get the values from the user selections
+	let xCoord = xInput.value;
+	let yCoord = yInput.value;
 
-		// Find max of y-coorindate point
-		const MAX_Y = d3.max(data, (d) => {
-			return parseInt(d.y)
-		}) 
+	// convert the values to the same ratio for the graph
+	let x = (xCoord * 50);
+	let y = 500 - (yCoord * 50);
 
-		// Scale for y 
-		const Y_SCALE = d3.scaleLinear()
-							.domain([0, (MAX_Y)])
-							.range([VIS_HEIGHT, 0]);
+	// add point
+	FRAME1.append("circle")
+	    	.attr("class", "point")
+	    	.attr("cx", (x + MARGINS.left))
+	    	.attr("cy", (y))
+	    	.attr("r", 10);
 
-		// Find max of x-coorindate point  
-		const MAX_X = d3.max(data, (d) => {
-			return parseInt(d.x)
-		});
+	// loop through all points
+	for (let i = 0; i < circles.length; i++) {
+	    
+	    // check each point for clicks
+	    let circle = circles[i];
+	    circle.addEventListener("click", pointClicked);
+		}
+	}
 
-		// Scale for x
-		const X_SCALE = d3.scaleLinear()
-							.domain([0, (MAX_X)])
+	// get the button Id
+	let pointButton =  document.getElementById("createButton");
+	// listen for a click
+	pointButton.addEventListener("click", addPoint);
+});
+
+	const FRAME2 = d3.select("#flex_left")
+					.append("svg")
+						.attr("height", FRAME_HEIGHT)
+						.attr("width", FRAME_WIDTH)
+						.attr("class", "frame");
+
+function build_barchart() {
+	// read data from second file
+	d3.csv("data/bar-data.csv").then((data) => {
+
+		console.log(data);
+
+		const MAX_Y2 = d3.max(data, (d) => {return parseInt(d.amount); });
+
+		const X_SCALE2 = d3.scaleBand()
+							.domain(data.map((d) => {return d.category}))
 							.range([0, VIS_WIDTH]);
 
-		// Title
-        FRAME1.append("text")
-	        .attr("x", MARGINS.left + VIS_WIDTH/2)
-	        .attr("y", MARGINS.top - 25)
-	        .attr('text-anchor', "middle")
-	        .style("font-size", 18)
-	        .text("Scatter Plot");
-        
-        // X label
-        FRAME1.append("text")
-	        .attr("x", MARGINS.left + VIS_WIDTH/2)
-	        .attr("y", VIS_HEIGHT + 125)
-	        .attr("text-anchor", "middle")
-	        .style("font-size", 12)
-	        .text("x");
-        
-        // Y label
-        FRAME1.append("text")
-	        .attr("text-anchor", "middle")
-	        .attr("x", MARGINS.left - 50)
-	        .attr("y", VIS_HEIGHT - 100)
-	        .style("font-size", 12)
-	        .text("y");
+		const Y_SCALE2 = d3.scaleLinear()
+							.range([VIS_HEIGHT, 0])
+							.domain([0, MAX_Y2])
 
-	    // Add tick marks for x axis
-		FRAME1.append("g")
-			.attr("transform", 
-				"translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")")
-			.call(d3.axisBottom(X_SCALE).ticks(9))
-				.attr("font-size", "10px");
-		
-		// Add tick marks for y axis 
-		FRAME1.append("g")
-			.attr("transform", 
-				"translate(" + MARGINS.left + "," + (MARGINS.bottom) + ")")
-			.call(d3.axisLeft(Y_SCALE).ticks(9))
-				.attr("font-size", "10px");
+		// plot
+		FRAME2.selectAll(".bar")
+						.data(data)
+		    			.enter().append("rect")
+		    						.attr("class", "bar")
+		    						.attr("x", d => {
+		    								return X_SCALE2(d.category) + MARGINS.left
+		    							})
+		    						.attr("y", d => {
+		    							return (Y_SCALE2(d.amount) + MARGINS.bottom)
+		    						})
+		    						.attr("width", X_SCALE2.bandwidth() - 5)
+		    						.attr("height", d => {
+		    							return (VIS_HEIGHT - Y_SCALE2(d.amount))
+		    						})
 
-		// Plot scatter plot
-        FRAME1.append("g")
-	        .selectAll("dot")
-	        .data(data)
-	        .enter()
-	        .append("circle")
-	        .attr("cx", (d) => {return X_SCALE(d.x) + MARGINS.left;})
-			.attr("cy", (d) => {return Y_SCALE(d.y) + MARGINS.top;})
-	        .attr("r", 10)
-	        .style("fill", "#CC0000")
-	        .attr("class", "point")
-	        .attr("stroke", "none");
+		// add x-axis
+		// create x-axis
+		FRAME2.append("g")
+	      		.attr("transform", "translate(" + 
+	      			MARGINS.left+ "," + (MARGINS.top + VIS_HEIGHT) + ")")
+	      			.call(d3.axisBottom(X_SCALE2).ticks(10));
 
-		// Add event listeners to the points for mouseover, mouseleave, and onclick
-		FRAME1.selectAll(".point")
-				.on("mouseover", handleMouseOver)
-				.on("mouseleave", handleMouseLeave)
-				.on("click", onClick); 
+		// create y-axis
+		FRAME2.append("g")
+	      		.attr("transform", "translate(" + 
+	      			MARGINS.left + "," + (MARGINS.top) + ")")
+	      		.call(d3.axisLeft(Y_SCALE2).ticks(10));
+
+	    // create a tooltip
+	    const TOOLTIP = d3.select("#flex_left")
+							.append("div")
+								.attr("class", "tooltip")
+								.style("opacity", 0);
+
+		// Handles mouse over functionality 
+		function handleMouseOver(event, d) {
+
+			TOOLTIP.style("opacity", 1);
+		}						
+
+		// Handles mouse move functionality 
+		function handleMouseMove(event, d) {
+
+			TOOLTIP.html("Name: " + d.category + "<br>Value: " + d.amount)
+					.style("left", (event.pageX + 10) + "px")
+					.style("top", (event.pageY - 50) + "px");
+		}
+
+		// Handles mouse leave functionality (when mouse is not hovering over a point)
+		function handleMouseLeave(event, d) {
+
+			TOOLTIP.style("opacity", 0);
+		}
+
+		// set the frame with the events
+		FRAME2.selectAll("rect")
+			.on("mouseover", handleMouseOver)
+			.on("mousemove", handleMouseMove)
+			.on("mouseleave", handleMouseLeave);
+
 	});
 }
-scatterPlot();
+
+build_barchart();
